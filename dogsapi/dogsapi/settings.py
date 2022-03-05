2ce30750461d0bb94338e9ffc5e1173db89b6bd3
Justin Wilcox <justinwilcox95@hotmail.com>
2022-03-04 18:24:53 -0800

    Update settings.py

--- dogsapi/dogsapi/settings.py
+++ dogsapi/dogsapi/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6$$0mc-(7avu-&q)y#f_(sf+&7rcf!-$(^^t8-m=(kcpg-_2y@'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
